8739c0d5878b9cb970c9065e1952292b50cbfcd8
shagun-gwl <shagun.maheshwari@goodworklabs.com>
2020-04-14 11:09:09 +0530

    layout Fix for employee login

--- web/rainmaker/packages/employee/src/modules/employee/User/Login/components/LoginForm/index.js
+++ web/rainmaker/packages/employee/src/modules/employee/User/Login/components/LoginForm/index.js
@@ -45,7 +45,7 @@ const LoginForm = ({ handleFieldChange, form, onForgotPasswdCLick, logoUrl,langu
           </div>
       */}
            <div style={{marginLeft:"7px" }}>
-              <Label bold={true} color="black" fontSize= "24px" label="STATE_LABEL" />
+              <Label bold={true} color="black" fontSize= "24px"  containerStyle={{height :30}} labelStyle={{height :30}} label="STATE_LABEL" />
            </div>
           </div>
           <Label style={{ marginBottom: "12px" }} className="text-center" bold={true} dark={true} fontSize={16} label="CORE_COMMON_LOGIN" />
@@ -66,8 +66,8 @@ const LoginForm = ({ handleFieldChange, form, onForgotPasswdCLick, logoUrl,langu
             </div>
           </Link>
           </div >
-          <div style={{ marginBottom: "10px", position: "relative", zIndex: 10,height: 30  }} className="text-right">
-              <div style={{ display: "inline-block" }}>
+          <div style={{ marginBottom: "10px", position: "relative", zIndex: 10,height: 40  }} className="text-right">
+              <div style={{ display: "inline-block", paddingTop : window.screen.availWidth > 770 ? 0 : 5 }}>
               <Label containerStyle={{ cursor: "pointer" }} id="otp-resend" className="otp-resend" label={languageSelected ?`LANGUAGE_${languageSelected.toUpperCase()}`:'en_IN'} />
               </div>
               <div style={{ display: "inline-block" , float:"right", height:40 }}>
