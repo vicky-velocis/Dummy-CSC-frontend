5ac3ca86234c90414c3fb983782d0ef831994002
saeel <saeel.parulekar@transerve.com>
2020-06-29 17:13:10 +0530

    PCR-39

---
+++ web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties/home.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { getCommonHeader } from "egov-ui-framework/ui-config/screens/specs/utils";
+import TradeLicenseIcon from "../../../../ui-atoms-local/Icons/TradeLicenseIcon";
+import "../utils/index.css";
+
+const header = getCommonHeader(
+  {
+    labelName: "Trade License",
+    labelKey: "TL_COMMON_TL"
+  },
+  {
+    classes: {
+      root: "common-header-cont"
+    }
+  }
+);
+
+const cardItems = [
+    {
+        label: {
+            labelKey: "RP_MASTER_PROPERTY_SEARCH",
+            labelName: "Master Properties"
+        },
+        icon: <TradeLicenseIcon />,
+        route: `search`
+    },
+    {
+        label: {
+            labelKey: "RP_TRANSFER_PROPERTY",
+            labelName: "Transfer Properties"
+        },
+        icon: <TradeLicenseIcon />,
+        route: `search`
+    },
+]
+
+const rentedPropertyHome = {
+  uiFramework: "material-ui",
+  name: "home",
+  beforeInitScreen: (action, state, dispatch) => {
+    return action;
+  },
+  components: {
+    div: {
+      uiFramework: "custom-atoms",
+      componentPath: "Div",
+      children: {
+        header: header,
+        applyCard: {
+          uiFramework: "custom-molecules",
+          componentPath: "LandingPage",
+          props: {
+            items: cardItems,
+            history: {}
+          }
+        },
+        listCard: {
+          uiFramework: "custom-molecules-local",
+          moduleName: "egov-tradelicence",
+          componentPath: "HowItWorks"
+        }
+      }
+    }
+  }
+};
+
+export default rentedPropertyHome;
