2aa6ea5f5fd1b016cee718017400bf70d03520ce
saeel <saeel.parulekar@transerve.com>
2020-07-01 16:16:15 +0530

    PCR-56

---
+++ web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties/search-mortgage.js
@@ -0,0 +1,63 @@
+import {
+    getCommonHeader,
+    getLabel,
+    getBreak,
+    getCommonContainer
+  } from "egov-ui-framework/ui-config/screens/specs/utils";
+import { setRoute } from "egov-ui-framework/ui-redux/app/actions";
+import { getQueryArg } from "egov-ui-framework/ui-utils/commons";
+import { prepareFinalObject } from "egov-ui-framework/ui-redux/screen-configuration/actions";
+import { localStorageGet,getTenantId } from "egov-ui-kit/utils/localStorageUtils";
+import { httpRequest } from "../../../../ui-utils";
+import find from "lodash/find";
+import get from "lodash/get";
+import { rentedPropertyApplication } from "./searchResource/rentedPropertyApplication";
+import { searchApiCall } from "./searchResource/functions"
+import { searchResults } from "./searchResource/searchResults";
+import { getColonyTypes } from "./apply";
+
+  const header = getCommonHeader({
+    labelName: "Mortgage",
+    labelKey: "MORTAGE_HEADER"
+  });
+  const transferPropertiesSearchAndResult = {
+    uiFramework: "material-ui",
+    name: "search",
+    beforeInitScreen: (action, state, dispatch) => {
+      dispatch(prepareFinalObject("searchScreen", {}))
+      getColonyTypes(action, state, dispatch)
+      searchApiCall(state, dispatch, true)
+      return action
+    },
+    components: {
+      div: {
+        uiFramework: "custom-atoms",
+        componentPath: "Form",
+        props: {
+          className: "common-div-css",
+          id: "search"
+        },
+        children: {
+          headerDiv: {
+            uiFramework: "custom-atoms",
+            componentPath: "Container",
+            children: {
+              header: {
+                gridDefination: {
+                  xs: 12,
+                  sm: 6
+                },
+                ...header
+              }
+            }
+          },
+          rentedPropertyApplication,
+          breakAfterSearch: getBreak(),
+          searchResults
+        }
+      }
+    }
+  };
+  
+  export default transferPropertiesSearchAndResult;
+  
