4e4c43e42dbc795c955265027efe75c28d44ee12
Tejaswi Katha <tejaswi.k@transerve.com>
2020-07-01 14:30:04 +0530

    documents in apply fresh licence

--- web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties-citizen/ownership-apply.js
+++ web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties-citizen/ownership-apply.js
@@ -3,16 +3,84 @@ import {
   } from "egov-ui-framework/ui-config/screens/specs/utils";
 import {stepper, formwizardOwnershipFirstStep, formwizardOwnershipSecondStep, formwizardOwnershipThirdStep } from '../rented-properties/applyResource/applyConfig';
 import {footer} from './footer';
+import { getMdmsData } from "../rented-properties/apply";
+import { prepareFinalObject } from "egov-ui-framework/ui-redux/screen-configuration/actions";
+import { prepareDocumentTypeObj } from "../utils";
+import { handleScreenConfigurationFieldChange as handleField } from "egov-ui-framework/ui-redux/screen-configuration/actions";
+import { get } from "lodash";
 
 const header = getCommonHeader({
     labelName: "Apply Fresh License",
     labelKey: "RP_COMMON_FRESH_LICENSE_APPLY"
 });
 
+const setDocumentData = async(action, state, dispatch) => {
+    const documentTypePayload = [{
+        moduleName: "PropertyServices",
+        masterDetails: [{name: "applications"}]
+      }
+    ]
+    const documentRes = await getMdmsData(dispatch, documentTypePayload);
+    const {PropertyServices} = !!documentRes && !!documentRes.MdmsRes ? documentRes.MdmsRes : {}
+    const {applications = []} = PropertyServices || {}
+    const findFreshLicenceItem = applications.find(item => item.code === "FRESHLICENSE")
+    const masterDocuments = !!findFreshLicenceItem ? findFreshLicenceItem.documentList : [];
+    const freshLicenceDocuments = masterDocuments.map(item => ({
+    type: item.code,
+    description: {
+      labelName: "Only .jpg and .pdf files. 6MB max file size.",
+      labelKey: item.fileType
+    },
+    formatProps :{
+      accept : item.accept || "image/*, .pdf, .png, .jpeg",
+    }, 
+    maxFileSize: 6000,
+    downloadUrl: item.downloadUrl,
+    moduleName: "RentedProperties",
+    statement: {
+        labelName: "Allowed documents are Aadhar Card / Voter ID Card / Driving License",
+        labelKey: item.description
+    }
+    }))
+    const documentTypes = prepareDocumentTypeObj(masterDocuments);
+    let applicationDocs = get(
+      state.screenConfiguration.preparedFinalObject,
+      "Properties[0].propertyDetails.applicationDocuments",
+      []
+    ) || [];
+    applicationDocs = applicationDocs.filter(item => !!item)
+    let applicationDocsReArranged =
+      applicationDocs &&
+      applicationDocs.length &&
+      documentTypes.map(item => {
+        const index = applicationDocs.findIndex(
+          i => i.documentType === item.name
+        );
+        return applicationDocs[index];
+      }).filter(item => !!item)
+    applicationDocsReArranged &&
+      dispatch(
+        prepareFinalObject(
+          "Properties[0].propertyDetails.applicationDocuments",
+          applicationDocsReArranged
+        )
+      );
+    dispatch(
+      handleField(
+          "ownership-apply",
+          "components.div.children.formwizardSecondStep.children.ownershipTransferDocumentsDetails.children.cardContent.children.documentList",
+          "props.inputProps",
+          freshLicenceDocuments
+      )
+  );
+    dispatch(prepareFinalObject("PropertiesTemp[0].applicationDocuments", documentTypes))
+}
+
 const applyLicense = {
     uiFramework: "material-ui",
     name: "ownership-apply",
     beforeInitScreen: (action, state, dispatch) => {
+        setDocumentData(action, state, dispatch)
         return action;
       },
     components: {
