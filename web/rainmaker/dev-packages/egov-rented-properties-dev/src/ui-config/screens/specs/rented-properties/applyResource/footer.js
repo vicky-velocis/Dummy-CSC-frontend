9b6813359a9cd919e9fc025ba109d614d2766501
priyanka <priyanka.talkar@transerve.com>
2020-07-16 18:47:00 +0530

    made changes to moveToSuccess function

--- web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties/applyResource/footer.js
+++ web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties/applyResource/footer.js
@@ -14,32 +14,23 @@ export const SUMMARY_STEP = 2;
 export const moveToSuccess = (rentedData, dispatch, type) => {
   const id = get(rentedData, "id");
   const transitNumber = get(rentedData, "transitNumber")
-  const applicationNumber = get(rentedData, "ownerDetails.applicationNumber")
+  const applicationNumber = get(rentedData, "applicationNumber")
+  const duplicateCopyApplicatioNumber =  get(rentedData, "applicationNumber")
   const tenantId = get(rentedData, "tenantId");
   const purpose = "apply";
   const status = "success";
+ 
   const path = type === "OWNERSHIPTRANSFERRP" ? 
   `/rented-properties/acknowledgement?purpose=${purpose}&status=${status}&applicationNumber=${applicationNumber}&tenantId=${tenantId}&type=${type}`
+  :type === "DUPLICATECOPYRP" ? 
+  `/rented-properties/acknowledgement?purpose=${purpose}&status=${status}&applicationNumber=${duplicateCopyApplicatioNumber}&tenantId=${tenantId}&type=${type}`
   : `/rented-properties/acknowledgement?purpose=${purpose}&status=${status}&transitNumber=${transitNumber}&tenantId=${tenantId}`
   dispatch(
     setRoute(path)
   );
 };
 
-export const dulicateCopyMoveToSuccess = (rentedData, dispatch, type) => {
-  const id = get(rentedData, "id");
-  const transitNumber = get(rentedData, "transitNumber")
-  const applicationNumber = get(rentedData, "Duplicate[0].applicationNumber")
-  const tenantId = get(rentedData, "tenantId");
-  const purpose = "apply";
-  const status = "success";
-  const path = type === "DUPLICATECOPYRP" ? 
-  `/rented-properties/acknowledgement?purpose=${purpose}&status=${status}&applicationNumber=${applicationNumber}&tenantId=${tenantId}&type=${type}`
-  : `/rented-properties/acknowledgement?purpose=${purpose}&status=${status}&transitNumber=${transitNumber}&tenantId=${tenantId}`
-  dispatch(
-    setRoute(path)
-  );
-};
+
 
 const callBackForNext = async(state, dispatch) => {
     let activeStep = get(
