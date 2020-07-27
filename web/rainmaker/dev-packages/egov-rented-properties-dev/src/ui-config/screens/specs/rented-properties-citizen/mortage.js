86803caed2efca97e2c5024922a78fcafd64785a
Tejaswi Katha <tejaswi.k@transerve.com>
2020-07-27 13:47:49 +0530

    mortgage workflow get bussinessservice data

--- web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties-citizen/mortage.js
+++ web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties-citizen/mortage.js
@@ -4,6 +4,7 @@ import FormIcon from "../../../../ui-atoms-local/Icons/FormIcon";
 import TradeLicenseIcon from "../../../../ui-atoms-local/Icons/TradeLicenseIcon";
 import "../utils/index.css";
 import { getTenantId } from "egov-ui-kit/utils/localStorageUtils";
+import { setBusinessServiceDataToLocalStorage } from "egov-ui-framework/ui-utils/commons";
 
 const tenantId = getTenantId();
 
@@ -37,9 +38,19 @@ const cardItems = [{
   }
 ]
 
+const getData = async (action, state, dispatch) => {
+  const queryObject = [{ key: "tenantId", value: getTenantId() }, 
+                      { key: "businessServices", value: "PermissionToMortgage" }]
+  await setBusinessServiceDataToLocalStorage(queryObject, dispatch);
+}
+
 const mortageHome = {
     uiFramework: "material-ui",
     name: "mortage",
+    beforeInitScreen: (action, state, dispatch) => {
+      getData(action, state, dispatch)
+      return action
+    },
     components: {
         div: {
           uiFramework: "custom-atoms",
