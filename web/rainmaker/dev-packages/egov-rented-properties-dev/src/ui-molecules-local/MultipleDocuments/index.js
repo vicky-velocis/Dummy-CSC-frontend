ce4acc9a5b27b62ff607158eb7d3c80341d0ec0d
Ruchira Naik <ruchira.naik@transerve.com>
2020-07-30 20:15:53 +0530

    added images in transit site tab and added download button

--- web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-molecules-local/MultipleDocuments/index.js
+++ web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-molecules-local/MultipleDocuments/index.js
@@ -31,90 +31,151 @@
 // export default withStyles(styles)(MultipleDocuments)
 
 import React, { Component } from "react";
-import Label from "egov-ui-framework/ui-containers/LabelContainer";
 import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography"
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import { checkValueForNA } from "egov-ui-framework/ui-config/screens/specs/utils";
-import get from "lodash/get";
+import classNames from "classnames";
+import Button from "@material-ui/core/Button";
+import {LabelContainer}  from "egov-ui-framework/ui-containers"
+// import "./index.css";
 
 const styles = {
     card: {
       paddingTop: 8,
       borderRadius: "inherit"
+    },
+    whiteCard: {
+      maxWidth: 250,
+      backgroundColor: "#FFFFFF",
+      paddingLeft: 8,
+      paddingRight: 0,
+      paddingTop: 11,
+      paddingBottom: 0,
+      marginRight: 16,
+      marginTop: 16
+    },
+    subtext: {
+      paddingTop: 7
+    },
+    body2: {
+      wordWrap: "break-word"
     }
   };
+  
+  const documentTitle = {
+    color: "rgba(0, 0, 0, 0.87)",
+    fontFamily: "Roboto",
+    fontSize: "16px",
+    fontWeight: 400,
+    letterSpacing: "0.67px",
+    lineHeight: "19px"
+  };
 
- class MultipleDocuments extends Component {
+//  class MultipleDocuments extends Component {
 
-    generateLabelKey = (content, item) => {
-        let LabelKey = "";
-        if (content.prefix && content.suffix) {
-          LabelKey = `${content.prefix}${get(item, content.jsonPath,"").replace(
-            /[._:-\s\/]/g,
-            "_"
-          )}${content.suffix}`;
-        } else if (content.prefix) {
-          LabelKey = `${content.prefix}${get(item, content.jsonPath,"").replace(
-            /[._:-\s\/]/g,
-            "_"
-          )}`;
-        } else if (content.suffix) {
-          LabelKey = `${get(item, content.jsonPath,"").replace(/[._:-\s\/]/g, "_")}${
-            content.suffix
-          }`;
-        } else if(content.callBack) {
-          LabelKey = content.callBack(get(item, content.jsonPath,""))
-        } else {
-          LabelKey = `${get(item, content.jsonPath,"")}`;
-        }
-        return LabelKey;
-      };
+//     render() {
+//         const {data = [], contents, classes} = this.props
+//         return (
+//             <div>
+//                 {!!data.length && data.map((datum, index) => (
+//                     <Card className={classes.card}>
+//                     <CardContent>
+//                     <Grid container>
+//                         {datum.applicationDocuments.map((content) => (
+//                             <Grid xs={6} sm={3} 
+//                             style={{
+//                                 marginBottom: "8px",
+//                                 marginTop: "8px",
+//                                 wordBreak : "break-word"
+//                               }}>
+//                             {/* <Grid xs={12} sm={12}>
+//                             <Typography variant="caption">
+//                             <Label
+//                               labelKey={content.fileStoreId}
+//                             //   fontSize={14}
+//                             />
+//                             </Typography>
+//                             </Grid> */}
+//                             <Grid xs={12} sm={12}>
+//                             <Typography variant="body2">
+//                             {/* <Label
+//                               labelKey={this.generateLabelKey(content, datum)}
+//                             //   fontSize={14}
+//                               checkValueForNA={checkValueForNA}
+//                             /> */}
+//                             </Typography>
+//                             </Grid>
+//                             </Grid>)
+//                         )}
+//                     </Grid>
+//                     </CardContent>
+//                     </Card>)
+//                     )}
+//             </div>
+//         )
+//     }
+// }
 
+class MultipleDocuments extends Component {
 
-    render() {
-        const {data = [], contents, classes} = this.props
-        return (
-            <div>
-                {!!data.length && data.map((datum, index) => (
-                    <Card className={classes.card}>
-                    <CardContent>
-                    <Grid container>
-                        {datum.applicationDocuments.map((content) => (
-                            <Grid xs={6} sm={3} 
-                            style={{
-                                marginBottom: "8px",
-                                marginTop: "8px",
-                                wordBreak : "break-word"
-                              }}>
-                            <Grid xs={12} sm={12}>
-                            <Typography variant="caption">
-                            <Label
-                              labelKey={content.fileStoreId}
-                            //   fontSize={14}
+  render() {
+      const {data = [], contents, classes} = this.props
+      return (
+          <div>
+              {!!data.length && data.map((datum, index) => (
+                  <Card className={classes.card}>
+                  <CardContent>
+                  <Grid container>
+                      {datum.applicationDocuments.map((content) => (
+                          <Grid xs={6} sm={3} 
+                          style={{
+                              marginBottom: "8px",
+                              marginTop: "8px",
+                              wordBreak : "break-word"
+                            }}>
+                           <Grid
+                            item
+                            container
+                            xs={6}
+                            sm={4}
+                            className={
+                               classNames(classes.whiteCard, "background-grey")
+                              
+                            }
+                            >
+                          <Grid xs={12}>
+                            <LabelContainer
+                              labelName= {content.documentType}
+                              labelKey="imagekey"
+                              style={documentTitle}
                             />
-                            </Typography>
+                          </Grid>
+                          <Grid container>
+                            <Grid xs={6} className={classes.subtext}>
+                              <Typography className={classes.body2}>{content.documentType}</Typography>
                             </Grid>
-                            <Grid xs={12} sm={12}>
-                            <Typography variant="body2">
-                            {/* <Label
-                              labelKey={this.generateLabelKey(content, datum)}
-                            //   fontSize={14}
-                              checkValueForNA={checkValueForNA}
-                            /> */}
-                            </Typography>
+                            <Grid xs={6} align="right">
+                              <Button href={content.url} color="primary">
+                                Download
+                              </Button>
                             </Grid>
-                            </Grid>)
-                        )}
-                    </Grid>
-                    </CardContent>
-                    </Card>)
-                    )}
-            </div>
-        )
-    }
+                          </Grid>
+                          </Grid>
+                      
+                          </Grid>)
+                      )}
+                  </Grid>
+                  </CardContent>
+                  </Card>)
+                  )}
+          </div>
+      )
+  }
 }
+
+
 
 export default withStyles(styles)(MultipleDocuments)
