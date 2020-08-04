bafb00d349f5a782fa62dabb0b422d9b75451476
Shagun Maheshwari <shagun.maheshwari@goodworklabs.com>
2020-08-04 13:29:26 +0530

    caching sector for PGR search

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/localStorageUtils/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/localStorageUtils/index.js
@@ -22,7 +22,9 @@ export const getLocale = () => {
 export const getDefaultLocale = () => {
   return localStorage.getItem("defaultLocale");
 };
-
+export const getPGRSector = () => {
+  return localStorage.getItem("PGRSector");
+};
 
 export const getapplicationType = () => {
   return localStorage.getItem("applicationType");
@@ -63,6 +65,9 @@ export const setReturnUrl = (url) => {
 
 export const setDefaultLocale = (locale) => {
   localStorageSet("defaultLocale", locale);
+};
+export const setPGRSector = (locale) => {
+  localStorageSet("PGRSector", locale);
 };
 
 export const setapplicationType = (applicationTypeName) => {
