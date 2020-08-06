7a939bb769c50baa1e2b6dd0bbd8f7f7e9e43c09
tejaswi-transerve <66254684+tejaswi-transerve@users.noreply.github.com>
2020-08-06 21:41:45 +0530

    Update index.js

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/ImageUpload/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/ImageUpload/index.js
@@ -51,7 +51,7 @@ class ImageUpload extends Component {
     }
     else{
       for (let i = 0; i < imageLength - images.length; i++) {
-        placeholders.push(<Placeholder key={i} inputProps={inputProps} onFilePicked={onFilePicked} hide={i === 1 ? false : false} />);
+        placeholders.push(<Placeholder key={i} inputProps={inputProps} onFilePicked={onFilePicked} hide={i === 1 ? true : false} />);
       }
     }
     return placeholders;
