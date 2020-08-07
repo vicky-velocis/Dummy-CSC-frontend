6e2d405b33a3e4dacd7b45fbfce3bbb94c2107ac
neeraj-transerve <neeraj.kamat@transerve.com>
2020-08-07 11:45:55 +0530

    search-preview-init

---
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/searchPreview.js
@@ -0,0 +1,207 @@
+import {
+    getCommonHeader,
+    getCommonContainer,
+    getLabel,
+    getCommonCard
+} from "egov-ui-framework/ui-config/screens/specs/utils";
+import { getQueryArg, setDocuments } from "egov-ui-framework/ui-utils/commons";
+import { prepareFinalObject } from "egov-ui-framework/ui-redux/screen-configuration/actions";
+import { getSearchResults } from "../../../../ui-utils/commons";
+import { setRoute } from "egov-ui-framework/ui-redux/app/actions";
+import { getReviewOwner, getReviewProperty, getReviewAddress, getReviewRentDetails, getReviewPaymentDetails,getReviewGrantDetails } from "./applyResource/review-property";
+import { getReviewDocuments } from "./applyResource/review-documents";
+import { getUserInfo ,getTenantId} from "egov-ui-kit/utils/localStorageUtils";
+
+const userInfo = JSON.parse(getUserInfo());
+const {roles = []} = userInfo
+const findItem = roles.find(item => item.code === "CTL_CLERK");
+
+let transitNumber = getQueryArg(window.location.href, "transitNumber");
+
+export const headerrow = getCommonContainer({
+  header: getCommonHeader({
+    labelName: "Rented Properties",
+    labelKey: "RP_COMMON_RENTED_PROPERTIES"
+  })
+});
+const reviewOwnerDetails = getReviewOwner(false);
+const reviewPropertyDetails = getReviewProperty(false);
+const reviewAddressDetails = getReviewAddress(false);
+const reviewRentDetails = getReviewRentDetails(false);
+const reviewPaymentDetails = getReviewPaymentDetails(false);
+const reviewDocumentDetails = getReviewDocuments(false, "apply")
+const reviewGrantDetails = getReviewGrantDetails(false)
+
+export const propertyReviewDetails = getCommonCard({
+  reviewPropertyDetails,
+  reviewAddressDetails,
+  reviewOwnerDetails,
+  reviewRentDetails,
+  reviewPaymentDetails,
+  reviewDocumentDetails,
+  reviewGrantDetails
+});
+
+export const searchResults = async (action, state, dispatch, transitNumber) => {
+  let queryObject = [
+    { key: "transitNumber", value: transitNumber }
+  ];
+  let payload = await getSearchResults(queryObject);
+  if(payload) {
+    let properties = payload.Properties;
+
+    let applicationDocuments = properties[0].propertyDetails.applicationDocuments || [];
+    const removedDocs = applicationDocuments.filter(item => !item.active)
+    applicationDocuments = applicationDocuments.filter(item => !!item.active)
+    properties = [{...properties[0], propertyDetails: {...properties[0].propertyDetails, applicationDocuments}}]
+    dispatch(prepareFinalObject("Properties[0]", properties[0]));
+    dispatch(
+      prepareFinalObject(
+        "PropertiesTemp[0].removedDocs",
+        removedDocs
+      )
+    );
+    await setDocuments(
+      payload,
+      "Properties[0].propertyDetails.applicationDocuments",
+      "PropertiesTemp[0].reviewDocData",
+      dispatch,'RP'
+    );
+  }
+}
+
+const beforeInitFn = async (action, state, dispatch, transitNumber) => {
+  dispatch(prepareFinalObject("workflow.ProcessInstances", []))
+  if(transitNumber){
+    await searchResults(action, state, dispatch, transitNumber)
+  }
+}
+
+export const onTabChange = async(tabIndex, dispatch, state) => {
+  transitNumber = getQueryArg(window.location.href, "transitNumber");
+  const tenantId = getQueryArg(window.location.href, "tenantId");
+  let path = ""
+
+  switch(tabIndex){
+
+  }
+  if(tabIndex === 0) {
+    path = `/rented-properties/search-preview?transitNumber=${transitNumber}&tenantId=${tenantId}`
+  } else if(tabIndex === 1) {
+    path = `/rented-properties/property-transitImages?transitNumber=${transitNumber}&tenantId=${tenantId}`
+  } else if(tabIndex === 2) {
+    path = `/rented-properties/notices?transitNumber=${transitNumber}&tenantId=${tenantId}`
+  }
+  dispatch(setRoute(path))
+}
+
+export const tabs = [
+  {
+    tabButton: { labelName: "Property Details", labelKey: "ESTATE_PROPERTY_DETAILS" },
+  },
+  {
+    tabButton: { labelName: "Owner Details", labelKey: "ESTATE_OWNER_DETAILS" },
+  },
+  {
+    tabButton: { labelName: "Purchaser Details", labelKey: "ESTATE_PURCHASER_DETAILS" },
+  },
+  {
+    tabButton: { labelName: "Payment Details", labelKey: "ESTATE_PAYMENT_DETAILS" },
+  },
+  {
+    tabButton: { labelName: "Court Case", labelKey: "ESTATE_COURT_CASE" },
+  },
+  {
+    tabButton: { labelName: "Notices", labelKey: "ESTATE_NOTICES" },
+  }
+]
+
+const estateDetailPreview = {
+  uiFramework: "material-ui",
+  name: "search-preview",
+  beforeInitScreen: (action, state, dispatch) => {
+    transitNumber = getQueryArg(window.location.href, "transitNumber");
+    // beforeInitFn(action, state, dispatch, transitNumber);
+    return action;
+  },
+  components: {
+    div: {
+      uiFramework: "custom-atoms",
+      componentPath: "Div",
+      props: {
+        className: "common-div-css search-preview"
+      },
+      children: {
+        headerDiv: {
+          uiFramework: "custom-atoms",
+          componentPath: "Container",
+          children: {
+            header1: {
+              gridDefination: {
+                xs: 12,
+                sm: 8
+              },
+             ...headerrow
+            },
+            searchButton: {
+              componentPath: "Button",
+              visible: !!findItem,
+              gridDefination: {
+                xs: 12,
+                sm: 4,
+                align: "right"
+              },
+              props: {
+                variant: "contained",
+                style: {
+                  color: "white",
+                  backgroundColor: "#fe7a51",
+                  borderColor:"#fe7a51",
+                  borderRadius: "2px",
+                  width: "50%",
+                  height: "48px",
+                }
+              },
+              children: {
+                buttonLabel: getLabel({
+                  labelName: "NOTICE RECOVERY",
+                  labelKey: "RP_NOTICE_RECOVERY"
+                })
+              },
+              onClickDefination: {
+                action: "condition",
+                callBack: (state, dispatch) => {
+                  dispatch(setRoute(`/rented-properties/notice-recovry?tenantId=${getTenantId()}`));
+                }
+              }
+            },
+            }
+          },
+          tabSection: {
+            uiFramework: "custom-containers-local",
+            moduleName: "egov-rented-properties",
+            componentPath: "CustomTabContainer",
+            props: {
+              tabs,
+              activeIndex: 0,
+              onTabChange
+            },
+            type: "array",
+          },
+          taskStatus: {
+            uiFramework: "custom-containers-local",
+            moduleName: "egov-rented-properties",
+            componentPath: "WorkFlowContainer",
+            props: {
+              dataPath: "Properties",
+              moduleName: "MasterRP",
+              updateUrl: "/csp/property/_update"
+            }
+          },
+        propertyReviewDetails
+      }
+    }
+  }
+};
+
+export default estateDetailPreview;
