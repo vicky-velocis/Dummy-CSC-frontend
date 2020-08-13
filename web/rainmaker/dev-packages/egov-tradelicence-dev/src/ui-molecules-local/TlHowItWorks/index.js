209a9ac8181b7028796dcd923dfba46bca288fe4
Rajiv Singaseni <rajiv@Rajivs-MacBook-Pro-2.local>
2020-08-13 23:23:20 +0530

    Use relative url for user manual. remove unused loadULBLogo code.

--- web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-molecules-local/TlHowItWorks/index.js
+++ web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-molecules-local/TlHowItWorks/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 
 export default class TlHowItWorks extends Component {
     render() {
-        const url = process.env.REACT_APP_MEDIA_BASE_URL + "/CTL_User_Manual_Citizen.pdf#view=FitH&embedded=true"
+        const url = process.env.REACT_APP_MEDIA_BASE_URL + "/TL_User_Manual_Citizen.pdf#view=FitH&embedded=true"
         return(
             <div style={{height: "100vh"}}>
             <iframe
