29f1af4f4d5478a0a134350f57d4090795c22d60
Tejaswi Katha <tejaswi.k@transerve.com>
2020-08-17 11:55:45 +0530

    json data for test application

--- web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate-citizen/apply.js
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate-citizen/apply.js
@@ -1,8 +1,10 @@
-import { getCommonHeader, getTextField, getCommonContainer, getCommonCard, getSelectField, getPattern, getDateField } from "egov-ui-framework/ui-config/screens/specs/utils";
-import { stepper } from '../estate/applyResource/applyConfig'
+import { getCommonHeader, getTextField, getCommonContainer, getCommonCard, getSelectField, getPattern, getDateField, getCommonTitle, getCommonParagraph } from "egov-ui-framework/ui-config/screens/specs/utils";
 import { prepareFinalObject, handleScreenConfigurationFieldChange as handleField } from "egov-ui-framework/ui-redux/screen-configuration/actions";
-import {dataFormat} from './sample.json'
 import { getTodaysDateInYMD } from "egov-ui-framework/ui-utils/commons";
+import {first_step} from './first_step.json';
+import {preview} from './preview.json';
+import {footer, stepper} from './footer'
+import { get } from "lodash";
 
 const header = getCommonHeader({
     labelName: "Apply",
@@ -19,6 +21,129 @@ const formwizardFirstStep = {
   }
 }
 
+const documentList = {
+  uiFramework: "custom-containers-local",
+  moduleName: "egov-estate",
+  componentPath: "DocumentListContainer",
+  props: {
+    buttonLabel: {
+      labelName: "UPLOAD FILE",
+      labelKey: "TL_BUTTON_UPLOAD FILE"
+    },
+    downloadButtonLabel: {
+      labelName: "DOWNLOAD FILE",
+      labelKey: "TL_BUTTON_DOWNLOAD_FILE"
+    },
+    inputProps : [],
+    documentTypePrefix: "EST_",
+    documentsJsonPath: "temp[0].documents",
+    uploadedDocumentsJsonPath: "temp[0].uploadedDocsInRedux",
+    tenantIdJsonPath: "ch.chandigarh",
+    removedJsonPath: "temp[0].removedDocs"
+  }
+};
+
+const documentDetails = getCommonCard({
+  header: getCommonTitle(
+    {
+      labelName: "Required Documents",
+      labelKey: "TL_NEW-UPLOAD-DOCS_HEADER"
+    },
+    {
+      style: {
+        marginBottom: 18
+      }
+    }
+  ),
+  paragraph: getCommonParagraph({
+    labelName:
+      "Only one file can be uploaded for one document. If multiple files need to be uploaded then please combine all files in a pdf and then upload",
+    labelKey: "TL_NEW-UPLOAD-DOCS_SUBHEADER"
+  }),
+  documentList
+})
+
+const formwizardSecondStep = {
+  uiFramework: "custom-atoms",
+  componentPath: "Form",
+  props: {
+    id: "apply_form2"
+  },
+  children: {
+    documentDetails
+  },
+  visible: false
+}
+
+const prepareDocuments = (documents, jsonPath) => {
+  let documentsArr =
+    documents.length > 0
+      ? documents.reduce((documentsArr, item, ind) => {
+        documentsArr.push({
+          name: item.code,
+          required: item.required,
+          jsonPath: `${jsonPath}[${ind}]`,
+          statement: item.description
+        });
+        return documentsArr;
+      }, [])
+      : [];
+  return documentsArr;
+};
+
+const setDocumentData = async(action, state, dispatch, {screenKey, screenPath}) => {
+  const {jsonPath, documentList = []} = require("./second_step.json");
+  const documents = documentList.map(item => ({
+    type: item.code,
+    description: {
+      labelName: "Only .jpg and .pdf files. 6MB max file size.",
+      labelKey: item.fileType
+    },
+    formatProps :{
+      accept : item.accept || "image/*, .pdf, .png, .jpeg",
+    }, 
+    maxFileSize: 6000,
+    downloadUrl: item.downloadUrl,
+    moduleName: "RentedProperties",
+    statement: {
+        labelName: "Allowed documents are Aadhar Card / Voter ID Card / Driving License",
+        labelKey: item.description
+    }
+    }))
+    const documentTypes = prepareDocuments(documentList, jsonPath);
+    let applicationDocs = get(
+      state.screenConfiguration.preparedFinalObject,
+      jsonPath,
+      []
+    ) || [];
+    applicationDocs = applicationDocs.filter(item => !!item)
+    let applicationDocsReArranged =
+      applicationDocs &&
+      applicationDocs.length &&
+      documentTypes.map(item => {
+        const index = applicationDocs.findIndex(
+          i => i.documentType === item.name
+        );
+        return applicationDocs[index];
+      }).filter(item => !!item)
+    applicationDocsReArranged &&
+      dispatch(
+        prepareFinalObject(
+          jsonPath,
+          applicationDocsReArranged
+        )
+      );
+    dispatch(
+      handleField(
+          screenKey,
+          screenPath,
+          "props.inputProps",
+          documents
+      )
+  );
+    dispatch(prepareFinalObject("temp[0].documents", documentTypes))
+}
+
 const headerObj = value => {
     return getCommonHeader({
         labelName: value,
@@ -28,7 +153,7 @@ const headerObj = value => {
 
 const getField = item => {
 
-  const {label: labelItem, placeHolder, type, pattern, editable = true, ...rest } = item
+  const {label: labelItem, placeholder, type, pattern, editable = true, ...rest } = item
   
   let fieldProps = {
     label : {
@@ -36,8 +161,8 @@ const getField = item => {
       labelKey: labelItem
     },
      placeholder : {
-      labelName: placeHolder,
-      labelKey: placeHolder
+      labelName: placeholder,
+      labelKey: placeholder
     },
      gridDefination : {
       xs: 12,
@@ -49,19 +174,19 @@ const getField = item => {
   fieldProps = !!pattern ? {...fieldProps, pattern: getPattern(pattern)} : fieldProps
 
   switch(type) {
-    case "text_field": {
+    case "TEXT_FIELD": {
       return getTextField({
         ...fieldProps,
         ...rest
     })
     }
-    case "dropDown": {
+    case "DROP_DOWN": {
       return getSelectField({
         ...fieldProps,
         ...rest
       })
     }
-    case "date": {
+    case "DATE_FIELD": {
       return getDateField({
         ...fieldProps,
         ...rest,
@@ -72,7 +197,7 @@ const getField = item => {
         pattern: getPattern("Date")
       })
     }
-    case "text_area": {
+    case "TEXT_AREA": {
       return getTextField({
         ...fieldProps,
         ...rest,
@@ -92,26 +217,29 @@ const getField = item => {
 
 const getDetailsContainer = ({fields = []}) => {
     const values = fields.reduce((acc, field) => {
-      return {...acc, [field.label.replace(/ /g, "_")]: getField(field)}
+      return {...acc, [field.label]: getField(field)}
     }, {})
     return getCommonContainer(values);
+}
+
+const setThirdStep = async (action, state, dispatch) => {
+  const {sections = []} = preview;
 }
 
 const getData = async (action, state, dispatch) => {
     await new Promise((resolve) => {
         setTimeout(resolve, 0)
     })
-    let { sections = [] } =  dataFormat;
+    let { sections = [] } =  first_step;
     sections = sections.reduce((acc, section) => {
         return {
         ...acc, 
-        [section.header.replace(/ /g, "_")]: getCommonCard({
+        [section.header]: getCommonCard({
             header: headerObj(section.header),
             details_container: getDetailsContainer(section)
         })
     }
     }, {})
-    console.log("====sections", sections)
     dispatch(
         handleField(
           "apply",
@@ -120,6 +248,20 @@ const getData = async (action, state, dispatch) => {
           sections
         )
       );
+    setDocumentData(action, state, dispatch, {screenKey: "apply", screenPath: "components.div.children.formwizardSecondStep.children.documentDetails.children.cardContent.children.documentList"})
+    setThirdStep(action, state, dispatch)
+}
+
+const formwizardThirdStep = {
+  uiFramework: "custom-atoms",
+  componentPath: "Form",
+  props: {
+    id: "apply_form3"
+  },
+  children: {
+    // reviewDetails
+  },
+  visible: false
 }
 
 const commonApply = {
@@ -151,7 +293,10 @@ const commonApply = {
               }
             },
             stepper,
-            formwizardFirstStep
+            formwizardFirstStep,
+            formwizardSecondStep,
+            formwizardThirdStep,
+            footer
           }
         }
       }
--- web/rainmaker/dev-packages/egov-estate-dev/src/ui-molecules-local/DocumentList/index.js
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-molecules-local/DocumentList/index.js
@@ -110,8 +110,9 @@ class DocumentList extends Component {
     }
     getQueryArg(window.location.href, "action") !== "edit" &&
       Object.values(uploadedDocumentsArranged).forEach((item, index) => {
-        const { jsonPath } = documents[index];
-        prepareFinalObject(
+        const documentType = item.documentType || item[0].documentType
+        const { jsonPath, name } = documents[index];
+        documentType === name && prepareFinalObject(
           jsonPath,
           { ...item[0] }
         );
