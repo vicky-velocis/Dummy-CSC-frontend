3187887eeb391b7c5063394dd0c4b52585db62df
priyanka <priyanka.talkar@transerve.com>
2020-08-17 18:31:14 +0530

    Father's/Husband's name is not displayed din DC summary/previewApplication charges entered by SA is displayed as zero for the SI/ next level users

--- web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties/ownership-search-preview.js
+++ web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties/ownership-search-preview.js
@@ -64,6 +64,7 @@ const tenantId = getQueryArg(window.location.href, "tenantId")
     ownershipTransferDocuments = ownershipTransferDocuments.filter(item => !!item.active)
     Owners = [{...Owners[0], ownerDetails: {...Owners[0].ownerDetails, ownershipTransferDocuments}}]
     const status = Owners[0].applicationState
+    const amount=Owners[0].ownerDetails.dueAmount
     dispatch(prepareFinalObject("Owners", Owners))
     dispatch(
       prepareFinalObject(
@@ -129,7 +130,7 @@ const tenantId = getQueryArg(window.location.href, "tenantId")
     process.env.REACT_APP_NAME === "Citizen"
         ? set(action, "screenConfig.components.div.children.footer", footer)
         : set(action, "screenConfig.components.div.children.footer", {});
-    const showEstimate = status !== "OT_DRAFTED" && status !== "OT_PENDINGCLVERIFICATION" && status !== "OT_PENDINGJAVERIFICATION" && status !== "OT_PENDINGSAVERIFICATION" && status!="OT_PENDINGCLARIFICATION"
+    const showEstimate = status !== "OT_DRAFTED" && status !== "OT_PENDINGCLVERIFICATION" && status !== "OT_PENDINGJAVERIFICATION" && status !== "OT_PENDINGSAVERIFICATION" && status!=="OT_PENDINGCLARIFICATION"&& status!=="OT_PENDINGSIVERIFICATION"&& status!=="OT_PENDINGCAAPPROVAL"&& status!="OT_PENDINGAPRO"
     dispatch(
       handleField(
           "ownership-search-preview",
