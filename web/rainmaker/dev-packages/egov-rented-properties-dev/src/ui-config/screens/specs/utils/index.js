9b4beb2a0c3d779bdfe439b822544fabc3dd6f87
Ruchira Naik <ruchira.naik@transerve.com>
2020-08-21 09:40:52 +0530

    due and charges coming as empty fixed

--- web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/utils/index.js
+++ web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/utils/index.js
@@ -1071,7 +1071,7 @@ export const download = (receiptQueryString, Owners, data, generateBy, mode = "d
       let {billAccountDetails} = Payments[0].paymentDetails[0].bill.billDetails[0];
       billAccountDetails = billAccountDetails.map(({taxHeadCode, ...rest}) => ({
         ...rest,
-        taxHeadCode: taxHeadCode.includes("_FEE") ? "RP_CHARGES" : taxHeadCode.includes("_PENALTY") ? "RP_PENALTY" : taxHeadCode.includes("_TAX") ? "RP_TAX" : taxHeadCode.includes("_ROUNDOFF") ? "RP_ROUNDOFF" : taxHeadCode.includes("_CHARGES") ? "RP_CHARGES"  : taxHeadCode
+        taxHeadCode: taxHeadCode.includes("_DUE") ? "RP_DUE" : taxHeadCode.includes("_PENALTY") ? "RP_PENALTY" : taxHeadCode.includes("_TAX") ? "RP_TAX" : taxHeadCode.includes("_ROUNDOFF") ? "RP_ROUNDOFF" : taxHeadCode.includes("_CHARGES") ? "RP_CHARGES"  : taxHeadCode
       }))
       Payments = [{...Payments[0], paymentDetails: [{...Payments[0].paymentDetails[0], bill: {...Payments[0].paymentDetails[0].bill, billDetails: [{...Payments[0].paymentDetails[0].bill.billDetails[0],billAccountDetails }] } }]}]
       httpRequest("post", DOWNLOADRECEIPT.GET.URL, DOWNLOADRECEIPT.GET.ACTION, queryStr, { Payments, generateBy }, { 'Accept': 'application/json' }, { responseType: 'arraybuffer' })
