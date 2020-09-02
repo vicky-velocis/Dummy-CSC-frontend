033efa3e91aa602ae303ff2ad46d9c77100ca0c8
Tejaswi Katha <tejaswi.k@transerve.com>
2020-09-03 00:26:59 +0530

    added tradelicence module and added localization for rp

--- web/rainmaker/packages/employee/src/ui-config/commonConfig/remote-config-paths/index.js
+++ web/rainmaker/packages/employee/src/ui-config/commonConfig/remote-config-paths/index.js
@@ -1,6 +1,11 @@
 const remoteConfigPath = (path, screenKey) => {
   let config = {};
   switch (path) {
+    case "tradelicence":
+    case "tradelicense-citizen":
+    case "fine-master":
+      config = require(`egov-tradelicence/ui-config/screens/specs/${path}/${screenKey}`).default;
+      break;
     case "rented-properties":
       config = require(`egov-rented-properties/ui-config/screens/specs/${path}/${screenKey}`).default;
       break;
