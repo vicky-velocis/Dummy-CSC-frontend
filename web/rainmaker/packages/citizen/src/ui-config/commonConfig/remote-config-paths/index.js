25f66564730ccaefa55a0975283b1fb8e2090ce2
Pooja Gawas <pooja.gawas@transerve.com>
2020-09-08 15:50:51 +0530

    fixed localisation and remote config path, remote component path issues

--- web/rainmaker/packages/citizen/src/ui-config/commonConfig/remote-config-paths/index.js
+++ web/rainmaker/packages/citizen/src/ui-config/commonConfig/remote-config-paths/index.js
@@ -1,6 +1,10 @@
 const remoteConfigPath = (path, screenKey) => {
   let config = {};
   switch (path) {
+    case "tradelicence":
+    case "tradelicense-citizen":
+      config = require(`egov-tradelicence/ui-config/screens/specs/${path}/${screenKey}`).default;
+      break;
     case "estate":
       config = require(`egov-estate/ui-config/screens/specs/${path}/${screenKey}`).default;
       break;
