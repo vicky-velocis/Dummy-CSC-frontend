d064000d4f187b59808e6008807453fa386a7850
Tejaswi Katha <tejaswi.k@transerve.com>
2020-09-08 12:28:19 +0530

    localization issue

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/commons.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/commons.js
@@ -833,7 +833,7 @@ export const getModuleName = () => {
   else if (pathName.indexOf("complaint") > -1 || pathName.indexOf("request-reassign") > -1 || pathName.indexOf("reassign-success") > -1) { return "rainmaker-pgr"; }
   else if (pathName.indexOf("wns") > -1) { return "rainmaker-ws"; }
   else if (pathName.indexOf("tradelicense") > -1 || pathName.indexOf("tradelicence") > -1 || pathName.indexOf("tradelicense-citizen") > -1 || pathName.indexOf("rainmaker-tl") > -1 ) { return "rainmaker-tl,rainmaker-pgr"; }
-  else if (pathName.indexOf("rented-properties") > -1) { return "rainmaker-rp,rainmaker-tl,rainmaker-pgr"}
+  else if (pathName.indexOf("rented-properties") > -1) { return "rainmaker-rp,rainmaker-tl,rainmaker-pgr,rainmaker-noc"}
   else if (pathName.indexOf("hrms") > -1) { return "rainmaker-hr"; }
   else if (pathName.indexOf("fire-noc") > -1) { return "rainmaker-noc,rainmaker-pgr"; }
   else if (pathName.indexOf("dss/home") > -1) { return "rainmaker-dss"; }
