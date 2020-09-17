088ee02e73b9883ab2228c0c62ab83c26fc7cb79
Tejaswi Katha <tejaswi.k@transerve.com>
2020-09-17 22:05:14 +0530

    catch block error fixed

--- web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-utils/apply.js
+++ web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-utils/apply.js
@@ -1036,7 +1036,7 @@ export const getRecoveryValueProperty = async (action,state, dispatch) => {
 
   }
  catch (error) {
-  console.log(e);
+  console.log(error);
   }
 }
 
@@ -1130,6 +1130,6 @@ export const getOfflineRentPaymentDetailsFromProperty = async (state, dispatch) 
     }
   }
  } catch (error) {
-  console.log(e);
+  console.log(error);
   }
 }
